6a7a2dadc05103fc66dfd7e95a6c0bb7c84d60c2
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-05 14:49:48 +0600

    style: tailwind configuration

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main>home</main>;
+  return <h1 className=" p-2 text-3xl font-bold bg-green-800">Hello world!</h1>;
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,16 +2,10 @@
 module.exports = {
   content: [
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
