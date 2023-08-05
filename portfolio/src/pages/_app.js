098e5ab13626556ccd8818f3db73c1a578b89c5b
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-05 22:11:18 +0600

    feat: add framer motion and create logo

--- portfolio/src/pages/_app.js
+++ portfolio/src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
+import Navbar from "../components/navbar";
 
 const monserrat = Montserrat({
   subsets: ["latin"],
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }) {
       <main
         className={`${monserrat.variable} font-mont bg-light w-full min-h-screen`}
       >
+        <Navbar />
         <Component {...pageProps} />
       </main>
     </>
