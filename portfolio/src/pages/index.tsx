6eee0b1cba9a53d0218d0a570f88acb551ef1a55
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-06 21:27:07 +0600

    feat: animated text

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from "@/components/layout";
 import Head from "next/head";
 import Image from "next/image";
 import profulePic from "../../public/images/profile/developer-pic-1.png";
+import AnimatedText from "@/components/animatedText";
 
 export default function Home() {
   return (
@@ -15,6 +16,18 @@ export default function Home() {
           <div className="flex items-center justify-between">
             <div className="w-1/2">
               <Image src={profulePic} alt="image" className=" h-auto" />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-6xl !text-left"
+              />
+              <p>
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development.
+              </p>
             </div>
           </div>
         </Layout>
