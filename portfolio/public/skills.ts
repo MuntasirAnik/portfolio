37130642828d04d11ac41914c78240fcd6bae5dc
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-08 22:37:03 +0600

    feat: skills added with map

---
+++ portfolio/public/skills.ts
@@ -0,0 +1,39 @@
+const skills = [
+    {
+        id: 1,
+        skill: "HTML",
+        src: "/../public/skills/html.png",
+    },
+    {
+        id: 2,
+        skill: "CSS",
+        src: "/../public/skills/css.png",
+    },
+    {
+        id: 3,
+        skill: "JAVASCRIPT",
+        src: "/../public/skills/javascript.png",
+    },
+    {
+        id: 4,
+        skill: "MONGODB",
+        src: "/../public/skills/mongo.png",
+    },
+    {
+        id: 5,
+        skill: "NEXTJS",
+        src: "/../public/skills/nextjs.png",
+    },
+    {
+        id: 6,
+        skill: "NODE JS",
+        src: "/../public/skills/node.png",
+    },
+    {
+        id: 7,
+        skill: "TAILWIND CSS",
+        src: "/../public/skills/tailwind.png",
+    },
+
+];
+export default skills;
--- portfolio/src/pages/skills.tsx
+++ portfolio/src/pages/skills.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 import html from "../../public/skills/html.png";
+import skillData from "../../public/skills"; // I assume skillData is an array of skill objects
 
 const Skills = () => {
   return (
@@ -10,17 +11,22 @@ const Skills = () => {
           skills
         </p>
         <h2 className="py-4">what I can do</h2>
-        <div className="grid md:grid-col-2 lg:grid-col-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 items-center justify-center">
-              <div className="m-auto">
-                <Image src={html} width="64" height="64" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {skillData.map((skill, index) => (
+            <div
+              key={index}
+              className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300"
+            >
+              <div className="grid grid-cols-2 gap-4 items-center justify-center">
+                <div className="m-auto">
+                  <Image src={skill.src} width={64} height={64} alt="HTML" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>{skill.skill}</h3>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
