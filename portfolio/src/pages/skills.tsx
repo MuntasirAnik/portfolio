4222c23a7b27c34e2739cbc925c846ca08f87a53
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-08 22:39:16 +0600

    feat: remove top padding

--- portfolio/src/pages/skills.tsx
+++ portfolio/src/pages/skills.tsx
@@ -6,7 +6,7 @@ import skillData from "../../public/skills"; // I assume skillData is an array o
 const Skills = () => {
   return (
     <div className="w-full lg:h-screen p-2">
-      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           skills
         </p>
