e0509c4d93b6aa6bdd30693e78fd7d6c3f30a138
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-08 21:37:16 +0600

    feat: add skill

--- portfolio/src/pages/skills.tsx
+++ portfolio/src/pages/skills.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import html from "../../public/skills/html.png";
 
 const Skills = () => {
   return (
     <div className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p>skills</p>
-        <h2>what I can do</h2>
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          skills
+        </p>
+        <h2 className="py-4">what I can do</h2>
+        <div className="grid md:grid-col-2 lg:grid-col-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 items-center justify-center">
+              <div className="m-auto">
+                <Image src={html} width="64" height="64" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
