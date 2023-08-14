007fd95136f3e89aee340446d4aaa698705911ae
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-14 22:11:32 +0600

    feat: navbar responsive

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./logo";
 import { useRouter } from "next/router";
 import LinkedIcon, { GithubIcon } from "./icons";
@@ -35,54 +35,177 @@ const CustomLink: React.FC<CustomLinkProps> = ({
   );
 };
 
+const CustomMobileLink: React.FC<CustomLinkProps> = ({
+  href,
+  title,
+  className = "",
+  toggle,
+}) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+  return (
+    <button
+      className={`${className} relative group text-light dark:text-dark my-2`}
+      onClick={handleClick}
+    >
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-light absolute left-0 -bottom-0.5 group-hover:w-full 
+        transition-[width] ease duration-300 dark:bg-dark
+        ${router.asPath === href ? "w-full" : "w-0"}`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
 const Navbar = () => {
   const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header
       className="w-full px-32 py-8 bg-light font-medium flex items-center justify-between dark:bg-dark
-    dark:text-light"
+    dark:text-light relative"
     >
-      <nav>
-        <CustomLink href={"/"} title="Home" className="mr-4" />
-        <CustomLink href={"/about"} title="About" className="mx-4" />
-        <CustomLink href={"/projects"} title="Projects" className="mx-4" />
-        <CustomLink
-          href={"/publications"}
-          title="Publications"
-          className="ml-4"
-        />
-      </nav>
+      <button
+        className="lg:flex hidden flex-col items-center justify-center"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 h-0.5 w-6 rounded-sm 
+        ${isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5 "}`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 h-0.5 w-6 rounded-sm translate-y-0.5 ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5 "
+          }`}
+        ></span>
+      </button>
+      <div className="w-full flex items-center justify-between lg:hidden">
+        <nav>
+          <CustomLink href={"/"} title="Home" className="mr-4" />
+          <CustomLink href={"/about"} title="About" className="mx-4" />
+          <CustomLink href={"/projects"} title="Projects" className="mx-4" />
+          <CustomLink
+            href={"/publications"}
+            title="Publications"
+            className="ml-4"
+          />
+        </nav>
 
-      <nav className="flex items-center justify-center flex-wrap">
-        <motion.a
-          href="https://www.linkedin.com/in/muntasir-kader-anik-620a27143/"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mr-3"
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://www.linkedin.com/in/muntasir-kader-anik-620a27143/"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mr-3"
+          >
+            <LinkedIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/MuntasirAnik"
+            target={"_blank"}
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mr-3 border border-solid dark:border-light rounded-lg"
+          >
+            <GithubIcon />
+          </motion.a>
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className="flex item-center justify-center rounded-full"
+          >
+            {mode === "dark" ? (
+              <BsFillSunFill className="fill-light" />
+            ) : (
+              <BsFillMoonStarsFill className="fill-dark" />
+            )}
+          </button>
+        </nav>
+      </div>
+
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] z-30 flex flex-col items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2
+    -translate-y-1/2 bg-dark/90 dark:bg-light/75 backdrop-blur-md py-32 rounded-lg"
         >
-          <LinkedIcon />
-        </motion.a>
-        <motion.a
-          href="https://github.com/MuntasirAnik"
-          target={"_blank"}
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mr-3 border border-solid dark:border-light rounded-lg"
-        >
-          <GithubIcon />
-        </motion.a>
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className="flex item-center justify-center rounded-full"
-        >
-          {mode === "dark" ? (
-            <BsFillSunFill className="fill-light" />
-          ) : (
-            <BsFillMoonStarsFill className="fill-dark" />
-          )}
-        </button>
-      </nav>
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href={"/"}
+              title="Home"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href={"/about"}
+              title="About"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href={"/projects"}
+              title="Projects"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href={"/publications"}
+              title="Publications"
+              className=""
+              toggle={handleClick}
+            />
+          </nav>
+
+          <nav className="flex items-center justify-center flex-wrap mt-2">
+            <motion.a
+              href="https://www.linkedin.com/in/muntasir-kader-anik-620a27143/"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mr-3"
+            >
+              <LinkedIcon />
+            </motion.a>
+            <motion.a
+              href="https://github.com/MuntasirAnik"
+              target={"_blank"}
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mr-3 border border-solid dark:border-light rounded-lg bg-light dark:bg-dark"
+            >
+              <GithubIcon />
+            </motion.a>
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className="flex item-center justify-center rounded-full"
+            >
+              {mode === "dark" ? (
+                <BsFillSunFill className="fill-light" />
+              ) : (
+                <BsFillMoonStarsFill className="fill-dark" />
+              )}
+            </button>
+          </nav>
+        </motion.div>
+      ) : null}
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
       </div>
