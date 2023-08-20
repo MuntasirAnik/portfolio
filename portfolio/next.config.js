9fe1aeff9b17c86ae2858b117c50b78c97e3e9d2
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-20 13:33:40 +0600

    delete configuration

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      loader: "imgix",
-      path: "/skills.ts", // This should match the public directory path
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
