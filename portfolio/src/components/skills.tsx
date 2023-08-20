5592270c7034ba79b7f4405173e7bb8fc461b658
MuntasirAnik <anik.muntasir005@gmail.com>
2023-08-20 13:57:30 +0600

    change skills file location

--- portfolio/src/components/skills.tsx
+++ portfolio/src/components/skills.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import skillData from "../../public/skills"; 
+import skillData from "../../data/skills"; 
 
 const Skills = () => {
   return (
